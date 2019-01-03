f47ca5bb8d6fe01e44fda958465411666a2b46ee
takahiro310 <sato.takahiro0811@gmail.com>
2019-01-03 15:36:00 +0900

    first commit

---
+++ src/main/java/com/takahiro310/demospringbootweb/DemoSpringBootWebApplication.java
@@ -0,0 +1,14 @@
+package com.takahiro310.demospringbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootWebApplication.class, args);
+	}
+
+}
+
